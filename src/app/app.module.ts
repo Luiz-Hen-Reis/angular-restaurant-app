9a4245369da2df46dd8a88746e6400892bf9790b
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-03 13:41:49 -0300

    chore: create home component scaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { AuthComponent } from './pages/auth/auth.component'
+import { AuthComponent } from './pages/auth/auth.component';
+import { HomeComponent } from './pages/home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AuthComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,

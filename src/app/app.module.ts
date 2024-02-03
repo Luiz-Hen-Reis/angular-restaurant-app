0d7d8134910e9298c3c30b066908812e2e80030f
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-02 21:00:58 -0300

    chore: declare AuthComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon';
+import { AuthComponent } from './pages/auth/auth.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,

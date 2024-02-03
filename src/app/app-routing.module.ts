10947a1500fd74dabf4566312cb82d32cbff3a7b
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-03 13:43:00 -0300

    chore: define home component path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'auth', component: AuthComponent }
 ];
 

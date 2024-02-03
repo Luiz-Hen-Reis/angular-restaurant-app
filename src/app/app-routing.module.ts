e13cbbe26cec9ec6bae2acc698198a29b52189ac
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-03 15:40:58 -0300

    chore: create routes for categories and checkout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'auth', component: AuthComponent }
+  { path: 'auth', component: AuthComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'checkout', component: CheckoutComponent },
 ];
 
 @NgModule({

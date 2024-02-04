d84f85910f1f31fbb110c55f94eeee59fcbc6598
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-04 15:35:27 -0300

    feat: implement getProducts to fetch the data to be displayed at home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RestaurantApiService } from 'src/app/services/restaurant-api.service';
+import { Product } from 'src/types/product';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  products: Product[] = [];
+  page: number = 1;
+  limit: number = 6;
 
+  constructor(private restaurantApiService: RestaurantApiService) {  }
+  apiUrl = 'http://localhost:4000'
+
+  ngOnInit() {
+    this.getProducts(this.page, this.limit);
+  }
+
+  getProducts(page = this.page,limit = this.limit) {
+  this.restaurantApiService.getProducts(page, limit).subscribe(products => {
+    this.products = products.products 
+  })
+  }
 }

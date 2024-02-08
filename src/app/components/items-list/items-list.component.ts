140b12803312749c0473c316f51299a323655194
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-08 20:06:44 -0300

    feat: create items-list component

--- src/app/components/items-list/items-list.component.ts
+++ src/app/components/items-list/items-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/types/product';
 
 @Component({
   selector: 'app-items-list',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./items-list.component.scss']
 })
 export class ItemsListComponent {
-
+  @Input() products: Product[] = [];
+  @Input() apiUrl: string = 'http://localhost:4000'
 }

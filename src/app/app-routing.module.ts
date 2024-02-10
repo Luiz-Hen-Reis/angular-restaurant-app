5b708ffd0777efdc46d98962f4ed848eab5c3112
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-10 16:21:18 -0300

    chore: create categories/:id route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AuthComponent } from './pages/auth/auth.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { CategoryComponent } from './pages/category/category.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', component: AuthComponent },
   { path: 'categories', component: CategoriesComponent },
+  { path: 'categories/:id', component: CategoryComponent },
   { path: 'checkout', component: CheckoutComponent },
 ];
 

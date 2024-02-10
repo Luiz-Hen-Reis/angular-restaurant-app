0d96d75e4469145225a0b62a8003809414cc2325
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-10 16:23:04 -0300

    feat: create navigateToCategory function

--- src/app/components/categories-list/categories-list.component.ts
+++ src/app/components/categories-list/categories-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Category } from 'src/types/category';
 
 @Component({
@@ -9,4 +10,10 @@ import { Category } from 'src/types/category';
 export class CategoriesListComponent {
   @Input() categories: Category[] = [];
   @Input() apiUrl: string = 'http://localhost:4000'
+
+  constructor(private router: Router) { }
+
+  navigateToCategory(id: string) {
+    this.router.navigate([`/categories/${id}`])
+  }
 }

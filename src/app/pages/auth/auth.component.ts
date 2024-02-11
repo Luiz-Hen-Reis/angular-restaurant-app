59278b1fbd5230eb9a34b6ceb9f187ec0807d5e0
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-11 19:07:14 -0300

    feat: create auth component initial logic

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
+  email: string = '';
+  password: string = '';
+  passwordConfirmation: string = '';
+  isRegister: boolean = true;
+
+  login() {
+  }
+  
+  register() {
+  }
+
+  toggleRegister() {
+    this.isRegister = !this.isRegister;
+  }
 
 }

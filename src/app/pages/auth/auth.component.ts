be1a02b6dc3f4aa0f5a5b4e69f1d0345aef8d445
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-12 17:39:35 -0300

    feat: implement login logic to auth

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserAuthentication } from 'src/types/user-authentication';
 
 @Component({
   selector: 'app-auth',
@@ -6,19 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
-  email: string = '';
-  password: string = '';
-  passwordConfirmation: string = '';
-  isRegister: boolean = true;
+  userRegistered: boolean = true;
+  userForm!: FormGroup;
+
+  constructor(private authServices: AuthService, private formBuilder: FormBuilder, private router: Router) { 
+    this.userForm = this.formBuilder.group({
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+    })
+   }
 
   login() {
+    const formData: UserAuthentication = this.userForm.value;
+    this.authServices.login(formData.email, formData.password);
   }
   
   register() {
+      // const formData: UserAuthentication = this.userForm.value
+      // this.authServices.registerNewUser(formData.name, formData.email,
+      //    formData.password, formData.passwordConfirmation).subscribe(response => {
+      //     console.log(response);
+      //    }
+      //    )
   }
 
   toggleRegister() {
-    this.isRegister = !this.isRegister;
+    this.userRegistered = !this.userRegistered;
   }
 
 }

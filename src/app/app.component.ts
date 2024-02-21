7397aa2b79105b2ee1befec86c347662fad5f017
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-21 15:57:24 -0300

    feat: add isTokenValid function to be handled onInit of app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { AuthService } from './services/auth.service';
+import { User } from 'src/types/user';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,20 @@ import { filter } from 'rxjs';
 export class AppComponent {
   title = 'Nakapoke';
   currentRoute: string = '';
+  user?: User;
 
-  constructor(private router: Router) { 
+  constructor(private router: Router, private authService: AuthService) { 
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)).subscribe(() => {
         this.currentRoute = this.router.url;
       })
    }
+
+   ngOnInit() {
+    this.authService.isTokenValid().subscribe(user => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  }
 }

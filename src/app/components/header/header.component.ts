0a444847c3df275ea801676d483ffadfecc02de7
Luiz Henrique <lhcontato2020@gmail.com>
2024-02-24 11:45:29 -0300

    feat: add token validation, create goToAuthPage function and logout function

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { User } from 'src/types/user';
 
@@ -10,5 +11,21 @@ import { User } from 'src/types/user';
 export class HeaderComponent {
   @Input() user?: User;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    this.authService.isTokenValid().subscribe(user => {
+      if (user) {
+        this.user = user;
+      }
+    })
+  }
+
+  goToAuthPage() {
+    this.router.navigate(['/auth']);
+  }
+
+  logout() {
+    this.authService.logout()
+  }
 }
